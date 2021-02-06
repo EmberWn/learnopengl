9ae33f3dde188d3d466a1d8dc16e2dcb12c818ba
wanghao <2289710829@qq.com>
2021-02-06 23:28:00 +0800

    texture

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -10,6 +10,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Shader.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.hpp"
 
 #include <iostream>
 #include <cmath>
@@ -59,11 +61,12 @@ int main(int argc, const char * argv[])
     }
 
     // vertex data
-    float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+   float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
     };
     // index data
     unsigned int indices[] = {  // note that we start from 0!
@@ -85,20 +88,71 @@ int main(int argc, const char * argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     
-    Shader out_shader("shader/shader.vs", "shader/shader.ps");
+    Shader our_shader("shader/shader.vs", "shader/shader.ps");
 
     int NumAttrbutes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &NumAttrbutes);
     std::cout << "maximum num of vertex attributes supported: " << NumAttrbutes << std::endl;
+    
+    int width, height, num_channels;
+    stbi_set_flip_vertically_on_load(true);  
+    unsigned char* data = stbi_load("textures/container.jpg", &width, &height, &num_channels, 0);
+    
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    
+    
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "FAILED TO LOAD TEXTURE" << std::endl;
+    }
+    
+    stbi_image_free(data);
+    
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    data = stbi_load("textures/awesomeface.png", &width, &height, &num_channels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    
+    
+    stbi_image_free(data);
+    
+    our_shader.Use(); // don't forget to activate the shader before setting uniforms!
+    our_shader.SetInt("texture1", 0);
+    our_shader.SetInt("texture2", 1); // or with shader class
 
     // render loop
     // -----------
@@ -117,7 +171,7 @@ int main(int argc, const char * argv[])
         
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         
-        out_shader.Use();
+        our_shader.Use();
 //        float time_value = glfwGetTime();
 //        float green_value = (sin(time_value) / 2.0f) + 0.5f;
 //        int vertex_color_location = glGetUniformLocation(ShaderProgram, "ourColor");
@@ -127,9 +181,14 @@ int main(int argc, const char * argv[])
 //        }
 //        glUniform4f(vertex_color_location, 0.0f, green_value, 0.0f, 1.0f);
         
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         glBindVertexArray(VAO);
-//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
         
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
---
+++ learnopengl/stb_image.cpp
@@ -0,0 +1,7 @@
+//
+//  stb_image.cpp
+//  learnopengl
+//
+//  Created by wangHao on 2021/2/6.
+//  Copyright © 2021 wangHao. All rights reserved.
+//
